2f10f1170ae589bc24fca139fa4e701e58e0024f
ayaosamaramadan <ayaramadan2011@gmail.com>
2026-02-25 02:46:36 +0200

    replace HelloWorld and TheWelcome components with Counterr component

---
+++ frontend/src/components/pages/Counterr.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+</script>
+
+<template>
+    <div class="counter">
+        <h1>Counter: {{ count }}</h1>
+        <button @click="count++">Increment</button>
+        <button @click="count--">Decrement</button>
+    </div>
+</template>
+
+<style scoped>
+.counter {
+    text-align: center;
+    margin-top: 50px;
+    margin-bottom: 50px;
+}
+</style>
