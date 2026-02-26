33572e2a5abf9265a332aad135dc567b19ec7ece
ayaosamaramadan <ayaramadan2011@gmail.com>
2026-02-26 22:48:36 +0200

    show tasks

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,10 +19,19 @@ onMounted(() => {
 </script>
 
 <template>
-  <input v-model="newTask" />
-  <button @click="addTask">Add</button>
+  <div>
+    <input v-model="newTask" />
+    <button @click="addTask">Add</button>
+  </div>
+
+  <div>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        <input type="checkbox" v-model="task.done" />
+        {{ task.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
